d7a1e3e0d1e7030834255bdd478f87953e0dea9b
Denis Smet <denis.smetannikov@yandex.ru>
2023-03-11 22:40:57 +0400

    New codestyle and PHP 8.1+

--- tests/fixtures/Custom/Filter.php
+++ tests/fixtures/Custom/Filter.php
@@ -1,17 +1,18 @@
 <?php
+
 /**
- * JBZoo Assets
+ * JBZoo Toolbox - Assets.
  *
- * This file is part of the JBZoo CCK package.
+ * This file is part of the JBZoo Toolbox project.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package   Assets
- * @license   MIT
- * @copyright Copyright (C) JBZoo.com,  All rights reserved.
- * @link      https://github.com/JBZoo/Assets
- * @author    Sergey Kalistratov <kalistratov.s.m@gmail.com>
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @see        https://github.com/JBZoo/Assets
  */
+
+declare(strict_types=1);
 
 namespace Custom\Assets;
 
